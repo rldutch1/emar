ed5d37dbabf77476fec7cbd77c156e9f73c00bde
Robert Holland <rob@robholland.com>
2014-12-07 12:35:36 -0700

    Latest admin for 7 Dec 2014.

---
+++ sql/update.sql
@@ -0,0 +1,2 @@
+INSERT INTO `h_mar` (`id`, `person_id`, `medication`, `dose`, `unit`, `admin_dttm`, `symptom`, `active_ind`) VALUES (NULL, '3', '7', '9', '3', '2014-12-07 08:10:00', 'Ear infection', '1'), (NULL, '3', '8', '2', '6', '2014-12-07 08:10:00', 'Pink eye', '1');
+
