067cbc740f332b1e4a8af6be03c2ba36fb7cfa54
rldutch1 <rob@robholland.com>
2014-12-08 10:26:26 -0700

    Added updates for 7 December 2014. Some of the updates have already been added to the online database and Gemini. These updates have not been added to the MacBook DB.

--- sql/update.sql
+++ sql/update.sql
@@ -1,2 +1,10 @@
-INSERT INTO `h_mar` (`id`, `person_id`, `medication`, `dose`, `unit`, `admin_dttm`, `symptom`, `active_ind`) VALUES (NULL, '3', '7', '9', '3', '2014-12-07 08:10:00', 'Ear infection', '1'), (NULL, '3', '8', '2', '6', '2014-12-07 08:10:00', 'Pink eye', '1');
+INSERT INTO `h_mar` (`id`, `person_id`, `medication`, `dose`, `unit`, `admin_dttm`, `symptom`, `active_ind`) 
+VALUES 
+(NULL, '3', '7', '9', '3', '2014-12-07 08:10:00', 'Ear infection', '1'), 
+(NULL, '3', '8', '2', '6', '2014-12-07 08:10:00', 'Pink eye', '1'),
+(NULL, '3', '8', '2', '6', '2014-12-07 12:13:00', 'Pink eye', '1'),
+(NULL, '3', '8', '2', '6', '2014-12-07 16:08:00', 'Pink eye', '1'),
+(NULL, '3', '8', '2', '6', '2014-12-07 20:10:00', 'Pink eye', '1'),
+(NULL, '3', '7', '9', '3', '2014-12-07 20:10:00', 'Ear infection', '1'), 
+(NULL, '3', '8', '2', '6', '2014-12-08 07:35:00', 'Pink eye', '1');
 
