b2d65e74923d14910905f469cbda203094c88e99
Robert Holland <rob@robholland.com>
2014-12-13 21:31:16 -0700

    Using jquery-1.11.0.min.js instead of 1.10.1.min.js. I also added the PHP file descriptor.

--- jsfooter.php
+++ jsfooter.php
@@ -1,6 +1,11 @@
+<?php
+// <script type='text/javascript' src='js/jquery-1.10.1.min.js'></script>
+echo "
+		<script src='//code.jquery.com/jquery-1.11.0.min.js'></script>
 
-		<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
 		<script type='text/javascript' src='js/jquery-ui-1.10.3.custom.js'></script>
-		<script type="text/javascript" src="js/jquery-ui-timepicker-addon.js"></script>
-		<script type="text/javascript" src="js/jquery-ui-sliderAccess.js"></script>
-		<script type="text/javascript" src="js/script.js"></script>
+		<script type='text/javascript' src='js/jquery-ui-timepicker-addon.js'></script>
+		<script type='text/javascript' src='js/jquery-ui-sliderAccess.js'></script>
+		<script type='text/javascript' src='js/script.js'></script>
+";
+?>
