a7c05a7f4a2b24eef65b6c889db9a1f0a6e2ab72
Robert Holland <rob@robholland.com>
2014-12-26 10:41:40 -0700

    Setup the  superglobal.

--- emar_post.php
+++ emar_post.php
@@ -2,12 +2,15 @@
 include('connect.php');
 include('../../includes/functions.php');
 
-if ($_POST['username'])
-namequery
-medquery
-dose
-unitquery
-admindate
-symptom
-temperature
+$x1 = $_POST['namequery'];
+$x2 = $_POST['medquery'];
+$x3 = $_POST['dose'];
+$x4 = $_POST['unitquery'];
+$x5 = $_POST['admindate'];
+$x6 = $_POST['symptom'];
+$x7 = $_POST['temperature'];
+
+echo   $x1 . " " . $x2 . " " . $x3 . " " . $x4 . " " . $x5 . " " . $x6 . " " . $x7;
+
+
 ?>
