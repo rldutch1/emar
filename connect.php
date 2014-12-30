070319286e7571d859f8c9e4e674ec14913ebeab
Robert Holland <rob@robholland.com>
2014-12-30 00:45:03 -0700

    Commented out the connection checking in the connect.php file.

--- connect.php
+++ connect.php
@@ -4,11 +4,12 @@ $db_user = 'emar';
 $db_password = 'emar';
 $db_dbname = 'emar';
 
-//$con=mysqli_connect("localhost","my_db","my_dbu","my_db");
 $con=mysqli_connect($db_server, $db_user, $db_password, $db_dbname);
+/* Connection checking is done with the confirm_queryi function in functions.php
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+*/
 ?>
