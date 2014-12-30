25d1d7f4e239b548108c43f63fcf58ad4f7dc93d
Robert Holland <rob@robholland.com>
2014-12-29 23:37:57 -0700

    Finally got it to display. I changed the path to the emar_post.php file in the emar.js file so that it will be relative to the caller instead of to the emar.js file. I also changed the div name in the emar.js file to "emardisplay" instead of "myDiv".

--- js/emar.js
+++ js/emar.js
@@ -88,7 +88,7 @@ else
   {
   	if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    document.getElementById("emardisplay").innerHTML=xmlhttp.responseText;
     }
   }
 
@@ -142,7 +142,7 @@ else if (isNaN(x7)==true || x7 < 0 || x7 == "")
 //alert(x1+" "+x2+" "+x3+" "+x4+" "+x5+" "+x6+" "+x7);
 
 //	xmlhttp.open("POST","../php/journalsubmit.php",true);
-	xmlhttp.open("POST","../emar_post.php",true);
+	xmlhttp.open("POST","emar_post.php",true);
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 //	xmlhttp.send("entryform="+x1+x2+x3+x4+x5+x6+x7+""); //Send the input from the HTML to the php file listed in the xmlhttp.open function.
 	xmlhttp.send("namequery="+x1+"&medquery="+x2+"&dose="+x3+"&unitquery="+x4+"&admindate="+x5+"&symptom="+x6+"&temperature="+x7+"");
