6b57423a12eedada5ca5c1761e780769c0599c7d
Robert Holland <rob@robholland.com>
2015-01-01 15:30:42 -0700

    Added logic to insert the last inserted id from the h_mar table into the h_temp table so that it will be easier to track which med administration coincides with a particular temperature.

--- emar_post.php
+++ emar_post.php
@@ -13,6 +13,7 @@ $x7 = $_POST['temperature'];
 $query="insert into h_mar (person_id, medication, dose, unit, admin_dttm, symptom) values
 				($x1, $x2, '" . $x3 ."', {$x4}, '" . $x5 . "', '" . $x6 . "');";
 
+
 /*
 This is working but I am testing last Insert ID below.
 			$result=mysqli_query($con, $query);
@@ -22,7 +23,13 @@ This is working but I am testing last Insert ID below.
 //Begin mysqli_insert_id testing.
 if (mysqli_query($con, $query)) {
     $last_id = mysqli_insert_id($con);
-    echo "New record created successfully. Last inserted ID is: " . $last_id;
+//    echo "New record created successfully. Last inserted ID is: " . $last_id;
+    $q1="Record added successfully.<br />";
+
+		$query2="insert into h_temp (temp_f, person_id, temp_dttm, symptom, hm_id) values ($x7, $x1, '" . $x5 . "', '" . $x6 . "', $last_id);";
+
+		if (mysqli_query($con, $query2)) { echo $q1 . "ID: " . $last_id;}
+
 } else {
     echo "Error: " . $query . "<br>" . mysqli_error($con);
 }
