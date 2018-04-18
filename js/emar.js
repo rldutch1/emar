8ed3763eee251ac96918ce163eee0e653f6cbe9c
Robert Holland <rob@robholland.com>
2018-04-17 20:00:07 -0700

    Added CSS to make the table look neater and inserted .js function to display patient data on submit.

--- js/emar.js
+++ js/emar.js
@@ -156,3 +156,27 @@ else if (isNaN(x7)==true || x7 < 0 || x7 == "")
 //xmlhttp.send("elementname="+xyzpdq1+"&nextelementname="+xyzpdq2+"");
 //Example: http://www.robholland.com/disneymovies.html
 }
+
+function showUser(str) {
+    if (str == '') {
+        document.getElementById('txtHint').innerHTML = '';
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById('txtHint').innerHTML = this.responseText;
+                alert(this.responseText);
+            }
+        };
+        xmlhttp.open('GET','patientshow.php?q='+str,true);
+        xmlhttp.send();
+    }
+}
+
